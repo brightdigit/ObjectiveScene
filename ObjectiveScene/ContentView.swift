ec65416b9e4bf6bdc723ef1fa0407ba36af3ae86
Leo Dion <leogdion@brightdigit.com>
2023-06-09 14:12:26 -0400

    added names

--- ObjectiveScene/ContentView.swift
+++ ObjectiveScene/ContentView.swift
@@ -12,12 +12,13 @@ import Charts
 struct ScenePieSet : Identifiable {
   let pies : [ScenePie]
   let id = UUID()
+  let title : String
   
   static func random (_ index : Int) -> ScenePieSet {
     ScenePieSet(pies:
                   (0...2).map({ index in
         .init(value: .random(in: 0...1))
-    })
+    }), title: setNames.randomElement()!
     )
   }
 }
@@ -45,52 +46,56 @@ struct ContentView: View {
   
     var body: some View {
         NavigationStack {
-          VStack{
-            GeometryReader(content: { geometry in
-              LazyVGrid(columns: [
-                GridItem(.adaptive(
-                  minimum: geometry.size.width / 4,
-                  maximum: geometry.size.width / 2
-                )
-                )
-              ], content: {
-                ForEach(sets) { set in
-                  ZStack{
-                    ForEach(0..<3) { index in
-                      
-                      if index < set.pies.count {
-                        let pie = set.pies[index]
-                        GeometryReader { proxy in
+          ScrollView{
+            VStack{
+              GeometryReader(content: { geometry in
+                LazyVGrid(columns: [
+                  GridItem(.adaptive(
+                    minimum: geometry.size.width / 4,
+                    maximum: geometry.size.width / 2
+                  )
+                  )
+                ], content: {
+                  ForEach(sets) { set in
+                    VStack{
+                      ZStack{
+                        ForEach(0..<3) { index in
                           
-                          Chart {
-                            SectorMark(
-                              angle:
-                                  .value("stuff", 1 - pie.value)
+                          if index < set.pies.count {
+                            let pie = set.pies[index]
+                            GeometryReader { proxy in
                               
-                            ).opacity(0.0)
+                              Chart {
+                                SectorMark(
+                                  angle:
+                                      .value("stuff", 1 - pie.value)
+                                  
+                                ).opacity(0.0)
+                                
+                                SectorMark(angle:
+                                    .value(pie.id.uuidString, pie.value),
+                                           innerRadius: MarkDimension(
+                                            floatLiteral:
+                                              (proxy.size.width / 2.0) / (3.0) * (3 - Double(index) - 1.0) + 5.0
+                                           ),
+                                           outerRadius: MarkDimension(
+                                            floatLiteral:
+                                              (proxy.size.width / 2.0) / (3.0) * (3 - Double(index))
+                                           )
+                                ).foregroundStyle(colors[index])
+                              }
+                            }.padding(8.0).frame(width: geometry.size.width / 3, height: geometry.size.width / 3, alignment: .center)
                             
-                            SectorMark(angle:
-                                .value(pie.id.uuidString, pie.value),
-                                       innerRadius: MarkDimension(
-                                        floatLiteral:
-                                          (proxy.size.width / 2.0) / (3.0) * (3 - Double(index) - 1.0) + 5.0
-                                                                 ),
-                                       outerRadius: MarkDimension(
-                                        floatLiteral:
-                                          (proxy.size.width / 2.0) / (3.0) * (3 - Double(index))
-                                                                 )
-                            ).foregroundStyle(colors[index])
                           }
-                        }.padding(8.0).frame(width: geometry.size.width / 3, height: geometry.size.width / 3, alignment: .center)
-                        
+                        }
                       }
+                      
+                      Text(set.title).fontWeight(.light)
                     }
                   }
-                }
+                })
               })
-            })
-            
-            Spacer()
+            }
           }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
